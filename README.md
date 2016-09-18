e1dd258467a32aca1febe38ff54429d3dae90185
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-09-18 22:13:42 +1000

    Added a console app to extract install count

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Go to lambda-uploader directory and execute following
 ```sh
 node --harmony_rest_parameters upload-lambda --function-name fetch-vscode-extension-stats --config ./config/vsc-extension-stats.json
 ```
+
+## Run app to get install counts
+
+WIP
+
+```sh
+$ node app --config path/to/config.json
+```
+
+```json
+{
+  "aws.region": "REGION",
+  "aws.accessKeyId": "ACCESS_KEY_ID",
+  "aws.secretAccessKey": "SECRET_ACCESS_KEY"
+}
+```
---
+++ app.js
@@ -0,0 +1,30 @@
+
+const aws = require('aws-sdk');
+const fs = require('fs');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+const config = JSON.parse(fs.readFileSync(argv.config, 'utf8'));
+const docClient = new aws.DynamoDB.DocumentClient({
+    region: config['aws.region'],
+    accessKeyId: config['aws.accessKeyId'],
+    secretAccessKey: config['aws.secretAccessKey']
+});
+
+const params = {
+    TableName : "vsc-extension-stats--stats",
+    KeyConditionExpression: "extensionId = :extId",
+    ExpressionAttributeValues: {
+        ":extId": '2100095e-ca9e-42ef-9bb4-5da1002c8139'
+    }
+};
+docClient.query(params).promise()
+    .then(data => data.Items.map(item => item.raw.statistics))
+    .then(items => items.map(
+        item => item.filter(attribute => attribute.statisticName === 'install')[0].value
+    ))
+    .then(stats => {
+        console.log(JSON.stringify(stats, null, 2));
+    }).catch(e => {
+        console.log(e);
+    });
