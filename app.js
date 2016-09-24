a4d54646c193f14b7b8e8e7cf60388217017270e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-09-24 23:15:22 +1000

    Change the format of csv

--- app.js
+++ app.js
@@ -12,18 +12,22 @@ const docClient = new aws.DynamoDB.DocumentClient({
     secretAccessKey: config['aws.secretAccessKey']
 });
 const csvStringifier = createCsvStringifier({
-    header: [
-        {id: 'id', title: 'EXTENSION ID'},
-        {id: 'name', title: 'EXTENSION NAME'},
+    header: [   // TODO: Get header values dynamically
         {id: 'date', title: 'DATE'},
-        {id: 'installs', title: 'INSTALL COUNT'}
+        {id: '2100095e-ca9e-42ef-9bb4-5da1002c8139', title: 'Annotator'},
+        {id: '79afa437-682e-4fa2-a4fd-87844832a1dc', title: 'Partial Diff'},
+        {id: 'ab840a62-e128-4f36-8587-2e52f4eecc06', title: 'Text Marker'},
+        {id: 'fa3848aa-3e65-4989-86e3-99d138c7f90b', title: 'Put Print'},
     ]
 });
 
 const params = {TableName : 'vsc-extension-stats--stats'};
+
+// TODO: Paging
 docClient.scan(params).promise()
     .then(data => data.Items)
     .then(items => items.map(getWriteObject))
+    .then(transformIntoDateRecords)
     .then(records => {
         console.log(csvStringifier.getHeaderString().trim());
         console.log(csvStringifier.stringifyRecords(records));
@@ -38,4 +42,19 @@ function getWriteObject(item) {
         installs: item.raw.statistics.filter(stat => stat.statisticName === 'install')[0].value,
         name: item.raw.displayName
     };
+}
+
+function transformIntoDateRecords(items) {
+    return items.reduce((dayRecords, item) => {
+        const dayRecord = dayRecords.find(dayRecord => dayRecord.date === item.date);
+        if (dayRecord) {
+            dayRecord[item.id] = item.installs;
+        } else {
+            dayRecords.push({
+                date: item.date,
+                [item.id]: item.installs
+            });
+        }
+        return dayRecords;
+    }, []);
 }
